c481b0c28c16d8058cf97db6b3c8a10eaa848cc7
yash001dev <y.sanghavi001@gmail.com>
2021-09-23 09:18:52 +0530

    Ng-rock configuration

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,41 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+
+if ENV["RACK_ENV"] == "development"
+  begin
+    options = {
+      addr: ENV.fetch("PORT") { 3000 },
+      config: File.join(ENV["HOME"], ".ngrok2", "ngrok.yml"),
+    }
+
+    # If you have a paid plan you can create tunnels with custom subdomains
+    options[:subdomain] = ENV.fetch("NGROK_SUBDOMAIN") { nil }
+    options[:region] = ENV.fetch("NGROK_REGION") { "us" }
+
+    # Create tunnel
+    Ngrok::Tunnel.start(options)
+
+    box = TTY::Box.frame(width: 50, height: 10, padding: 2,
+                         title: { top_left: "<NGROK>", bottom_right: "</NGROK>" },
+                         style: { fg: :green, bg: :black, border: { fg: :green, bg: :black } }) do
+      [
+        "STATUS: #{Ngrok::Tunnel.status}",
+        "PORT:   #{Ngrok::Tunnel.port}",
+        "HTTP:   #{Ngrok::Tunnel.ngrok_url}",
+        "HTTPS:  #{Ngrok::Tunnel.ngrok_url_https}",
+      ].join("\n")
+    end
+  rescue => error
+    box = TTY::Box.frame(width: 50, height: 5, align: :center, padding: 1,
+                         title: { top_left: "<NGROK>", bottom_right: "</NGROK>" },
+                         style: { fg: :red, bg: :black, border: { fg: :red, bg: :black } }) do
+      "ERROR: could not create a tunnel ;("
+    end
+  end
+  puts "\n#{box}\n"
+end
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
