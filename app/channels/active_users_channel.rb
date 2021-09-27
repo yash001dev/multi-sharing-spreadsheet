9d30007b418227ac63d33cc0230eb5ae70bed0ee
yash001dev <y.sanghavi001@gmail.com>
2021-09-27 09:03:43 +0530

    Active User Addedd

--- app/channels/active_users_channel.rb
+++ app/channels/active_users_channel.rb
@@ -1,9 +1,12 @@
 class ActiveUsersChannel < ApplicationCable::Channel
+  include NoBrainer::Streams
+
   def subscribed
-    # stream_from "some_channel"
+    @user = User.create
+    stream_from User.all, include_initial: true
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    @user.destroy
   end
 end
